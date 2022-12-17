8ffd5b35816191cdb6bca6dbb2bd75f6b4f3b4f3
dootheg <dootheg@daum.net>
2022-12-16 21:20:29 -0800

    initial build success.

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module logmon
 
 go 1.19
 
-require (
-    github.com/takama/daemon
-)
+require github.com/takama/daemon v1.0.0
+
+require golang.org/x/sys v0.0.0-20200722175500-76b94024e4b6 // indirect
--- main.go
+++ main.go
@@ -1,3 +1,24 @@
-
 //
-idfdfkjdflakdjf
+// Log Monitor DaeMon
+//
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/takama/daemon"
+)
+
+func main() {
+	service, err := daemon.New("name", "description", daemon.SystemDaemon)
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+	status, err := service.Install()
+	if err != nil {
+		log.Fatal(status, "\nError: ", err)
+	}
+	fmt.Println(status)
+}
